256a49663628db7aab85f9c4f2a625ce2c19d05a
pavlo.v <pavlo.v@kakaocorp.com>
2025-07-10 10:59:16 +0900

    update configs to actuator

--- loans/src/main/java/com/eazybytes/loans/dto/LoansContactInfoDto.java
+++ loans/src/main/java/com/eazybytes/loans/dto/LoansContactInfoDto.java
@@ -1,11 +1,17 @@
 package com.eazybytes.loans.dto;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import java.util.List;
 import java.util.Map;
 
+@Getter
+@Setter
 @ConfigurationProperties(prefix = "loans")
-public record LoansContactInfoDto(String message, Map<String, String> contactDetails, List<String> onCallSupports) {
-
+public class LoansContactInfoDto {
+    String message;
+    Map<String, String> contactDetails;
+    List<String> onCallSupport;
 }
